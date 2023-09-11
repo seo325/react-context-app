fe4392cd3dda4fe9fb85c0cb0a8b33c4182fbee6
seohwa0325 <seohwa0325@gmail.com>
2023-09-11 12:43:45 +0900

    server connect

--- src/compoents/Type.js
+++ src/compoents/Type.js
@@ -1,7 +1,35 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Products from './Products'
+import Options from './Options'
 const Type = ({ orderType }) => {
-    console.log(orderType)
+
+    const [itmes, setItmes] = useState([])
+
+
+    useEffect((orderType) => {
+        loadItems(orderType)
+    }, [orderType])
+
+    const loadItems = async () => {
+        try {
+            const response = await axios.get(`http://localhost:4000/${orderType}`)
+            setItmes(response.data)
+
+        }
+        catch (error) {
+            console.log(error)
+
+        }
+    }
+    const ItemComponent = orderType === "products" ? Products : Options
+
+    const optionsItems = itmes.map(item =>(
+        <ItemComponent 
+        key={item.name}
+        name = {item.name}
+        imagePath ={item.imagePath} />
+    ))
     return (
         <div>
             <h2> 주문 종류</h2>
@@ -9,8 +37,8 @@ const Type = ({ orderType }) => {
             <p>총 가격</p>
             <div
                 style={{ display: 'flex', flexDirection: orderType === "options" ? "column" : "row" }}
-                >
-                    Items
+            >
+                {optionsItems}
 
 
             </div>
